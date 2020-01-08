715cc3c9cd7bc3d5053eb2baac4eebf41a418d5f
Emanuel de Jong <edejong01@student.rocvantwente.nl>
2020-01-08 09:31:45 +0100

    Add project files.

---
+++ MBO-Open/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MBO_Open.MvcApplication" Language="C#" %>
---
+++ MBO-Open/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MBO_Open.Startup))]
+namespace MBO_Open
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
