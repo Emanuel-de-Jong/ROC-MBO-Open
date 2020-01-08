47d59a8b156af32cd71ca4ad3950b393bbc755a7
Emanuel de Jong <edejong01@student.rocvantwente.nl>
2020-01-08 18:05:37 +0100

    push

---
+++ MBO-Open/Controllers/ScholenController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MBO_Open.Models;
+
+namespace MBO_Open.Controllers
+{
+    public class ScholenController : Controller
+    {
+        private MBOOpenEntities db = new MBOOpenEntities();
+
+        // GET: Scholen
+        public ActionResult Index(string message="")
+        {
+            ViewBag.message = message;
+            return View(db.Scholens.ToList());
+        }
+
+        // GET: Scholen/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scholen scholen = db.Scholens.Find(id);
+            if (scholen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scholen);
+        }
+
+        // GET: Scholen/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Scholen/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "ID,Naam")] Scholen scholen)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Scholens.Add(scholen);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(scholen);
+        //}
+
+        // GET: Scholen/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scholen scholen = db.Scholens.Find(id);
+            if (scholen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(scholen);
+        }
+
+        // POST: Scholen/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Naam")] Scholen scholen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(scholen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(scholen);
+        }
+
+        // GET: Scholen/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            var message = "";
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Scholen scholen = db.Scholens.Find(id);
+            if (scholen == null)
+            {
+                return HttpNotFound();
+            }
+
+            if(scholen.Spelers.Count == 0)
+            {
+                db.Scholens.Remove(scholen);
+                db.SaveChanges();
+            }
+            else
+            {
+                message = "De school heeft spelers dus kan niet verwijdert worden.";
+            }
+
+            return RedirectToAction("Index", new { message = message });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MBO-Open/Controllers/SpelersController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MBO_Open.Models;
+
+namespace MBO_Open.Controllers
+{
+    public class SpelersController : Controller
+    {
+        private MBOOpenEntities db = new MBOOpenEntities();
+
+        // GET: Spelers
+        public ActionResult Index()
+        {
+            var spelers = db.Spelers.Include(s => s.Scholen);
+            return View(spelers.ToList());
+        }
+
+        // GET: Spelers/Create
+        public ActionResult Create()
+        {
+            ViewBag.SchoolID = new SelectList(db.Scholens, "ID", "Naam");
+            return View();
+        }
+
+        // POST: Spelers/Create.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Voornaam,Tussenvoegsels,Achternaam,SchoolID")] Speler speler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Spelers.Add(speler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SchoolID = new SelectList(db.Scholens, "ID", "Naam", speler.SchoolID);
+            return View(speler);
+        }
+
+        // GET: Spelers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speler speler = db.Spelers.Find(id);
+            if (speler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SchoolID = new SelectList(db.Scholens, "ID", "Naam", speler.SchoolID);
+            return View(speler);
+        }
+
+        // POST: Spelers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Voornaam,Tussenvoegsels,Achternaam,SchoolID")] Speler speler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(speler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SchoolID = new SelectList(db.Scholens, "ID", "Naam", speler.SchoolID);
+            return View(speler);
+        }
+
+        // GET: Spelers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speler speler = db.Spelers.Find(id);
+            if (speler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(speler);
+        }
+
+        // POST: Spelers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Speler speler = db.Spelers.Find(id);
+            db.Spelers.Remove(speler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
